76caafd5c43608f8799cf2c696bed39ddeeabf7c
aaradhyakul <aaradhyakul@gmail.com>
2023-06-30 17:14:38 +0530

    readiness & healthz routes

--- main.go
+++ main.go
@@ -1,8 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	chi "github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
  
 
 func main(){
-	fmt.Println("hello world")
+	godotenv.Load()
+	portString:=os.Getenv("PORT")
+	fmt.Println("hello")
+	if portString==""{
+		log.Fatal("PORT not found")
+	}
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"http://*", "https://*"},
+		AllowedMethods: []string{"GET","POST","PUT","DELETE","PATCH"},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: false,
+		MaxAge: 300,
+
+
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz",handlerReadiness)
+	v1Router.Get("/err",handlerErr)
+
+	router.Mount("/v1",v1Router)
+
+
+	srv := &http.Server{
+		Handler:router,
+		Addr:":" + portString,
+	}
+
+	log.Printf("Server starting on port:%v",portString)
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
